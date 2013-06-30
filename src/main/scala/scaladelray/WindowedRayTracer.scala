80ef7c78c6a6eb6610821c12376b12f172071be9
Stephan Rehfeld <rehfeld@beuth-hochschule.de>
2013-06-30 12:23:23 +0200

    Removed some debug messages

--- src/main/scala/scaladelray/WindowedRayTracer.scala
+++ src/main/scala/scaladelray/WindowedRayTracer.scala
@@ -21,7 +21,6 @@ import swing.{Component, MainFrame, SimpleSwingApplication}
 import java.awt.{Graphics2D, Dimension}
 import java.awt.image.BufferedImage
 import akka.actor.{Props, Actor, ActorSystem}
-import scaladelray.math.Ray
 
 import akka.pattern.ask
 import akka.util.Timeout
@@ -33,7 +32,6 @@ case class Render( startX : Int, startY : Int, width : Int, height : Int, cam : 
 
 class RenderingActor( world : World, id : Int ) extends Actor {
 
-  println( "Rendering Actor Started: " + id )
   def receive = {
     case msg : Render =>
       val pixel = scala.collection.mutable.MutableList[(Int,Int,Int)]()
@@ -55,8 +53,7 @@ abstract class WindowedRayTracer extends SimpleSwingApplication {
   def world : World
   def camera : ((Int,Int) => Camera)
 
-  //val actors = for( i <- 1 to 8 ) yield actorSystem.actorOf( Props( new RenderingActor(( world ), i )) )
-  val targets = actorSystem.actorOf( Props( new RenderingActor( world, 0 ) ).withRouter( RoundRobinRouter( nrOfInstances = 8 ) ) )
+  val targets = actorSystem.actorOf( Props( new RenderingActor( world, 0 ) ).withRouter( RoundRobinRouter( nrOfInstances = Runtime.getRuntime.availableProcessors() ) ) )
 
 
   implicit val timeout = Timeout(5 minutes)
@@ -78,15 +75,11 @@ abstract class WindowedRayTracer extends SimpleSwingApplication {
 
         var futures = scala.collection.mutable.MutableList[Future[Any]]()
 
-        print( "Creating jobs: " )
         val linesPerActor = this.size.getHeight.asInstanceOf[Int] / 8
         for( i <- 0 until 8 ) {
           val ftr = targets ? Render( 0, i * linesPerActor, this.size.getWidth.asInstanceOf[Int],  (i+1) * linesPerActor, cam )
           futures += ftr
         }
-
-
-        println( "done" )
 
         for( future <- futures ) {
           val pixel = Await.result( future, timeout.duration ).asInstanceOf[List[(Int,Int,Int)]]
