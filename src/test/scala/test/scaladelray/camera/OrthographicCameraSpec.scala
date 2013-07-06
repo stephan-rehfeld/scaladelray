cbc00eb57543f30964ca377e9e79c4d688a8b0c9
Stephan Rehfeld <rehfeld@beuth-hochschule.de>
2013-07-06 11:38:28 +0200

    Tests about usage of the sampling pattern in the orth cam

--- src/test/scala/test/scaladelray/camera/OrthographicCameraSpec.scala
+++ src/test/scala/test/scaladelray/camera/OrthographicCameraSpec.scala
@@ -17,8 +17,9 @@
 package test.scaladelray.camera
 
 import org.scalatest.FunSpec
-import scaladelray.math.{Ray, Vector3, Point3}
+import scaladelray.math.{Point2, Ray, Vector3, Point3}
 import scaladelray.camera.OrthographicCamera
+import scaladelray.sampling.SamplingPattern
 
 
 class OrthographicCameraSpec extends FunSpec {
@@ -27,6 +28,29 @@ class OrthographicCameraSpec extends FunSpec {
     it( "should calculate the correct center for an image with the size of 1024x768 and a size of 1") {
       val cam = new OrthographicCamera( Point3( 0, 0, 0 ), Vector3( 0, 0, -1 ), Vector3( 0, 1, 0 ), 1024, 768, 1 )
       assert( cam( 511, 383 ) == Set( Ray( Point3( 0, 0, 0 ), Vector3( 0, 0, -1 ) ) ) )
+    }
+
+    it( "should calculate the correct amount of rays for the given sampling pattern" ) {
+      val cam1 = new OrthographicCamera( Point3( 0, 0, 0 ), Vector3( 0, 0, -1 ), Vector3( 0, 1, 0 ), 400, 400, 400 )
+      assert( cam1( 0, 0 ).size == 1 )
+
+
+      val cam2 = new OrthographicCamera( Point3( 0, 0, 0 ), Vector3( 0, 0, -1 ), Vector3( 0, 1, 0 ), 400, 400, 400, SamplingPattern.regularPattern( 3, 2 ) )
+      assert( cam2( 0, 0 ).size == 3 * 2 )
+    }
+
+    it( "should use the points of the sampling pattern to generate correct altered ray of the primary ray of the pixel" ) {
+      val sp = SamplingPattern( Set() + Point2( 0, 0 ) + Point2( -0.5, -0.5 ) + Point2( 0.5, -0.5 ) + Point2( 0.5, 0.5 ) + Point2( -0.5, 0.5 ) )
+      val cam = new OrthographicCamera( Point3( 0, 0, 0 ), Vector3( 0, 0, -1 ), Vector3( 0, 1, 0 ), 400, 400, 400, sp )
+      val rays = cam( 199, 199 )
+      println( rays )
+      assert( rays.contains( Ray( Point3( 0, 0, 0 ), Vector3( 0, 0, -1 ) ) ) )
+      assert( rays.contains( Ray( Point3( 0.5, 0.5, 0 ), Vector3( 0, 0, -1 ) ) ) )
+      assert( rays.contains( Ray( Point3( -0.5, 0.5, 0 ), Vector3( 0, 0, -1 ) ) ) )
+      assert( rays.contains( Ray( Point3( -0.5, -0.5, 0 ), Vector3( 0, 0, -1 ) ) ) )
+      assert( rays.contains( Ray( Point3( 0.5, -0.5, 0 ), Vector3( 0, 0, -1 ) ) ) )
+
+
     }
   }
 
