1253474bd207d656757d21779496324b62cea535
Stephan Rehfeld <rehfeld@beuth-hochschule.de>
2013-08-31 12:20:56 +0200

    Unit test for loading a OBJ file that contains comments

--- src/test/scala/test/scaladelray/loader/OBJLoaderSpec.scala
+++ src/test/scala/test/scaladelray/loader/OBJLoaderSpec.scala
@@ -295,6 +295,55 @@ class OBJLoaderSpec extends FunSpec {
       assert( mesh.material == mat )
     }
 
+    it( "should load a file with comments that contains a cube with all vertices, texture coordinates, and normals into a triangle mesh" ) {
+      val objLoader = new OBJLoader()
+      val mesh = objLoader.load( "cube-v-vt-vn-comments.obj", SingleColorMaterial( Color( 0, 0, 0 ) ) )
+
+      assert( mesh.vertices.contains( v1 ) )
+      assert( mesh.vertices.contains( v2 ) )
+      assert( mesh.vertices.contains( v3 ) )
+      assert( mesh.vertices.contains( v4 ) )
+
+      assert( mesh.vertices.contains( v5 ) )
+      assert( mesh.vertices.contains( v6 ) )
+      assert( mesh.vertices.contains( v7 ) )
+      assert( mesh.vertices.contains( v8 ) )
+
+      assert( mesh.texCoords.contains( t1 ) )
+      assert( mesh.texCoords.contains( t2 ) )
+      assert( mesh.texCoords.contains( t3 ) )
+      assert( mesh.texCoords.contains( t4 ) )
+
+      assert( mesh.normals.contains( n1 ) )
+      assert( mesh.normals.contains( n2 ) )
+      assert( mesh.normals.contains( n3 ) )
+      assert( mesh.normals.contains( n4 ) )
+      assert( mesh.normals.contains( n5 ) )
+      assert( mesh.normals.contains( n6 ) )
+
+      val triangleSets = for( face <- mesh.faces ) yield
+        (mesh.vertices( face( 0 )._1 ) :: mesh.vertices( face( 1 )._1 ) :: mesh.vertices( face( 2 )._1 ) :: Nil, mesh.texCoords( face( 0 )._2.get ) :: mesh.texCoords( face( 1 )._2.get ) :: mesh.texCoords( face( 2 )._2.get ) :: Nil, mesh.normals( face( 0 )._3.get ) :: mesh.normals( face( 1 )._3.get ) :: mesh.normals( face( 2 )._3.get ) :: Nil  )
+
+      assert( triangleSets.contains( (v1 :: v2 :: v3 :: Nil, t1 :: t2 :: t3 :: Nil, n5 :: n5 :: n5 :: Nil ) ) )
+      assert( triangleSets.contains( (v3 :: v4 :: v1 :: Nil, t3 :: t4 :: t1 :: Nil, n5 :: n5 :: n5 :: Nil ) ) )
+      assert( triangleSets.contains( (v2 :: v6 :: v7 :: Nil, t1 :: t2 :: t3 :: Nil, n1 :: n1 :: n1 :: Nil ) ) )
+      assert( triangleSets.contains( (v7 :: v3 :: v2 :: Nil, t3 :: t4 :: t1 :: Nil, n1 :: n1 :: n1 :: Nil ) ) )
+      assert( triangleSets.contains( (v6 :: v5 :: v8 :: Nil, t1 :: t2 :: t3 :: Nil, n6 :: n6 :: n6 :: Nil ) ) )
+      assert( triangleSets.contains( (v8 :: v7 :: v6 :: Nil, t3 :: t4 :: t1 :: Nil, n6 :: n6 :: n6 :: Nil ) ) )
+      assert( triangleSets.contains( (v5 :: v1 :: v4 :: Nil, t1 :: t2 :: t3 :: Nil, n2 :: n2 :: n2 :: Nil ) ) )
+      assert( triangleSets.contains( (v4 :: v8 :: v5 :: Nil, t3 :: t4 :: t1 :: Nil, n2 :: n2 :: n2 :: Nil ) ) )
+      assert( triangleSets.contains( (v4 :: v8 :: v5 :: Nil, t3 :: t4 :: t1 :: Nil, n2 :: n2 :: n2 :: Nil ) ) )
+      assert( triangleSets.contains( (v4 :: v3 :: v7 :: Nil, t1 :: t2 :: t3 :: Nil, n3 :: n3 :: n3 :: Nil ) ) )
+      assert( triangleSets.contains( (v7 :: v8 :: v4 :: Nil, t3 :: t4 :: t1 :: Nil, n3 :: n3 :: n3 :: Nil ) ) )
+      assert( triangleSets.contains( (v5 :: v6 :: v2 :: Nil, t1 :: t2 :: t3 :: Nil, n4 :: n4 :: n4 :: Nil ) ) )
+      assert( triangleSets.contains( (v2 :: v1 :: v5 :: Nil, t3 :: t4 :: t1 :: Nil, n4 :: n4 :: n4 :: Nil ) ) )
+
+      assert( mesh.vertices.size == 8 )
+      assert( mesh.faces.size == 12 )
+      assert( mesh.texCoords.size == 4 )
+      assert( mesh.normals.size == 6 )
+    }
+
   }
 
 }
