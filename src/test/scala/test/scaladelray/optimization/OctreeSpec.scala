43fe37e368a7cb06df46606bee53a49c1c342b7d
Stephan Rehfeld <rehfeld@beuth-hochschule.de>
2013-09-12 15:22:18 +0200

    Declared the tests for the Octree

---
+++ src/test/scala/test/scaladelray/optimization/OctreeSpec.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2013 Stephan Rehfeld
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test.scaladelray.optimization
+
+import org.scalatest.FunSpec
+
+class OctreeSpec extends FunSpec {
+
+  describe( "An Octree" ) {
+    it( "accept a set that contains 0 octants" )(pending)
+    it( "should accept a set that contains 8 octants" )(pending)
+    it( "should throw an exception if a different amount than 0 or 8 octants are passed" )(pending)
+    it( "should provide the passed data" )(pending)
+
+
+  }
+
+}
