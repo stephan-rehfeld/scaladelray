cb20c9123c4191bd2751df17ecdae1ed45dbdf2b
Stephan Rehfeld <rehfeld@beuth-hochschule.de>
2015-06-02 13:41:05 +0200

    a new material class that aggregates BSDFs and light emission

---
+++ src/main/scala/scaladelray/material/Material.scala
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2015 Stephan Rehfeld
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package scaladelray.material
+
+/**
+ * A material aggreates several BSDFs and optionally contains a emission.
+ *
+ * @param e An optional light emission of the material. Default value is none.
+ * @param bsdfs BSDFs that describe this material.
+ */
+case class Material( e : Option[Emission] = None, bsdfs : BSDF* )
